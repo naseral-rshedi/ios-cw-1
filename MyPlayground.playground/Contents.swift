bf8be80ad5e54929a30eb7280de2a8b6c42fff6c
naser <bvif8n@gmail.com>
2020-06-13 19:18:15 +0300

    نص المشروع

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "naser"
+var height = 1.70
+var age = 17
+var hobby1 = "swimming"
+var hobby2 = "drawing"
+print("my name is",name, "my age is" ,age, "my  first hobby is" ,hobby1, "my second hobby is" ,hobby2)
