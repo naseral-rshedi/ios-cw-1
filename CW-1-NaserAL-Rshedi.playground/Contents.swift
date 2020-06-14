59767893f970cabfd3b6a53107387fb29c750df7
naser <bvif8n@gmail.com>
2020-06-14 17:30:15 +0300

    تم حل التمرين ١

---
+++ CW-1-NaserAL-Rshedi.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "naser"
+var height = 1.70
+var age = 17
+var hobby1 = "swimming"
+var hobby2 = "drawing"
+print("my name is",name, "my age is" ,age, "my  first hobby is" ,hobby1, "my second hobby is" ,hobby2)
+
+var friendname = "ahmed"
+var friendhobby1 = "drawing"
+var friendhobby2 = "walking"
+var friendage = 19
+var friendheight = 1.75
+print("my frienf name is :" ,friendname, "my friend age is :" ,friendage, ", my friend height is :" ,friendheight , "and his hobbies are" ,friendhobby1 , "," ,friendhobby2)
+
+var age_difference = (friendage - age)
+
+print(age_difference)
+
+name == friendname
+name > friendname
+name < friendname
+name >= friendname
+name <= friendname
+name != friendname
+
+hobby1 == friendhobby1
+hobby1 > friendhobby1
+hobby1 < friendhobby1
+hobby1 >= friendhobby1
+hobby1 <= friendhobby1
+hobby1 != friendhobby1
+
+hobby2 == friendhobby2
+hobby2 < friendhobby2
+hobby2 > friendhobby2
+hobby2 >= friendhobby2
+hobby2 <= friendhobby2
+hobby2 != friendhobby2
+
+age == friendage
+age > friendage
+age < friendage
+age >= friendage
+age <= friendage
+age != friendage
+
+height == friendheight
+height > friendheight
+height < friendheight
+height >= friendheight
+height <= friendheight
+height != friendheight
